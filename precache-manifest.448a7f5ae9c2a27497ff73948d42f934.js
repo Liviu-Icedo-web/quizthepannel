ce52000ff6030178d384b0a410bcdb979bae8cf2
Liviu Todoran <liviutodoran@Livius-MacBook-Pro.local>
2018-12-02 23:32:45 +0100

    Updates

---
+++ precache-manifest.448a7f5ae9c2a27497ff73948d42f934.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "11d03a501e85bebd3bd6",
+    "url": "/quizthepannel/static/js/runtime~main.11d03a50.js"
+  },
+  {
+    "revision": "cbf52e0a872512071e41",
+    "url": "/quizthepannel/static/js/main.cbf52e0a.chunk.js"
+  },
+  {
+    "revision": "c1c19f80e42f0654b727",
+    "url": "/quizthepannel/static/js/1.c1c19f80.chunk.js"
+  },
+  {
+    "revision": "cbf52e0a872512071e41",
+    "url": "/quizthepannel/static/css/main.030d76b3.chunk.css"
+  },
+  {
+    "revision": "206abd46c488c507d018d840df61826a",
+    "url": "/quizthepannel/index.html"
+  }
+];
